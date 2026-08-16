036c58ca6abec9f27cab4026c6dd1a67d7bcdffc
sarthak14457 <poudelsarthak44@gmail.com>
2026-08-16 21:09:09 +0545

    Update admin.js

--- frontend/admin.js
+++ frontend/admin.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3001/api";
+const API_BASE = "https://web-project-server-xu4m.onrender.com/api";
 
 const params = new URLSearchParams(window.location.search);
 const token = params.get("token");
