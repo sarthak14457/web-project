bb1ab57c4f9b61c23ecf751b2cc6286b44b072c1
sarthak14457 <poudelsarthak44@gmail.com>
2026-08-16 21:11:18 +0545

    Update login.js

--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3001/api";
+const API_BASE = "https://web-project-server-xu4m.onrender.com/api";
 const form = document.getElementById("loginForm");
 const errorMsg = document.getElementById("errorMsg");
 const successMsg = document.getElementById("successMsg");
